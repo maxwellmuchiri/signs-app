ba156d221999f653c4c81ea36bb88e6f2c662708
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-08-17 17:59:13 +0300

    fixes

--- signs-app-frontend/src/Commponents/Footer.js
+++ signs-app-frontend/src/Commponents/Footer.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { FaFacebookSquare, FaWhatsapp, FaEnvelope, FaPhone } from 'react-icons/fa';
 
@@ -8,10 +9,10 @@ function Footer() {
         <div className="row">
           <div className="col-md-12">
             <div className="footer-icons">
-              <a href="https://www.facebook.com/SaganaFruitsNursery " target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
-              <a href="https://wa.me/254792068962?text=I%27m%20interested%20in%20buying%20seedlings%20from%20your%20farm" target="_blank" rel="noreferrer"><FaWhatsapp /></a>
-              <a href="mailto:muchirimaxwel@gmail.com"><FaEnvelope /></a>
-              <a href="tel:+254792068962"><FaPhone /></a>
+              <a href="https://www.facebook.com/SaganaFruitsNursery" target="_blank" rel="noopener noreferrer">Visit Example<FaFacebookSquare /></a>
+              <a href="https://wa.me/254792068962?text=I%27m%20interested%20in%20buying%20seedlings%20from%20your%20farm" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
+              <a href="mailto:muchirimaxwel@gmail.com" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
+              <a href="tel:+254792068962" target="_blank" rel="noopener noreferrer"><FaPhone /></a>
             </div>
             <p>&copy; 2021 Farmer's Seedlings. All rights reserved.</p>
           </div>
@@ -22,3 +23,4 @@ function Footer() {
 }
 
 export default Footer;
+
